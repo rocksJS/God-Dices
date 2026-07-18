aef4f75e214d833c0e7f3b89b045e40e43017747
рокс__) <99092643+rocksJS@users.noreply.github.com>
2026-07-19 01:25:16 +0300

    fixed paths before build, fixed naming of folder with utility classes

---
+++ src/main/java/com/huepampalo/utility/SoulCage.java
@@ -0,0 +1,20 @@
+package com.huepampalo.classes;
+
+import java.util.UUID;
+
+import net.minecraft.world.phys.Vec3;
+
+public class SoulCage {
+
+    public final UUID owner;
+    public final Vec3 center;
+    public final double radius;
+    public int ticks;
+
+    public SoulCage(UUID owner, Vec3 center, double radius, int ticks) {
+        this.owner = owner;
+        this.center = center;
+        this.radius = radius;
+        this.ticks = ticks;
+    }
+}
